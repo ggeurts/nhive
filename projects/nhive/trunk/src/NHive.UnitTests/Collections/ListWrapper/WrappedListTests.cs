dafb50fdea55562e1697f970c06035334a972f84
ggeurts <ggeurts@652ee814-f92c-0410-a85a-9bc41fe80994>
2007-07-16 00:23:59 +0000

    Renaming ListWrapper to WrappedList

---
+++ projects/nhive/trunk/src/NHive.UnitTests/Collections/ListWrapper/WrappedListTests.cs
@@ -0,0 +1,82 @@
+namespace NHive.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using MbUnit.Framework;
+    using NHive.Base.Size;
+
+    [TypeFixture(
+        typeof(WrappedListTestArgs<string>))]
+    [ProviderFactory(
+        typeof(WrappedListFactory),
+        typeof(WrappedListTestArgs<string>))]
+    public class WrappedListTests 
+        : ListTestsBase<string, int, WrappedList<string>>
+    {
+        private static string[] _items = new string[] { "item 1", "item 2", "item 3" };
+        private static Random _random = new Random(0);
+
+        protected override string CreateRandomItem()
+        {
+            return _items[_random.Next(_items.Length - 1)];
+        }
+    }
+
+    [TypeFixture(
+        typeof(WrappedListTestArgs<string>))]
+    [ProviderFactory(
+        typeof(WrappedListFactory), 
+        typeof(WrappedListTestArgs<string>))]
+    public class WrappedListIteratorTests
+        : RandomAccessIteratableTestsBase<string, int, WrappedList<string>, WrappedList<string>.Iterator>
+    { }
+
+    internal class WrappedListTestArgs<T>
+        : ListTestArgs<T, int, WrappedList<T>>
+        , IRandomAccessIteratableTestArgs<T, int, WrappedList<T>, ArrayList32<T>.Iterator>
+    {
+        public WrappedListTestArgs(WrappedList<T> hive, IEnumerable<T> expectedItems)
+            : base(hive, expectedItems)
+        { }
+    }
+
+    internal class WrappedListFactory
+    {
+        [Factory]
+        public WrappedListTestArgs<string> EmptyStringList
+        {
+            get { return CreateWrappedListTestArgs<string>(); }
+        }
+
+        [Factory]
+        public WrappedListTestArgs<string> NonEmptyStringList
+        {
+            get { return CreateWrappedListTestArgs("#1", "#two", "#tres", "#vier"); }
+        }
+
+        [Factory]
+        public WrappedListTestArgs<string> EmptyStringArray
+        {
+            get { return CreateWrappedArrayTestArgs<string>(); }
+        }
+
+        [Factory]
+        public WrappedListTestArgs<string> NonEmptyStringArray
+        {
+            get { return CreateWrappedArrayTestArgs("#1", "#two", "#tres", "#vier"); }
+        }
+
+        private WrappedListTestArgs<T> CreateWrappedListTestArgs<T>(params T[] items)
+        {
+            WrappedList<T> wrappedList = new WrappedList<T>(
+                new List<T>(items));
+            return new WrappedListTestArgs<T>(wrappedList, items);
+        }
+
+        private WrappedListTestArgs<T> CreateWrappedArrayTestArgs<T>(params T[] items)
+        {
+            WrappedList<T> wrappedList = new WrappedList<T>(items);
+            return new WrappedListTestArgs<T>(wrappedList, items);
+        }
+    }
+}
